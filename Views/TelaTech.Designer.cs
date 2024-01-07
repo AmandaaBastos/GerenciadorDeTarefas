c443c4fb59f1b304bf3a0f1ea699e48970897435
AmandaaBastos <amandabastoscorreia@gmail.com>
2024-01-07 13:08:03 -0300

    fix(telas):Havia trocado o nome das telas tech e dev

---
+++ Views/TelaTech.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace GerenciadorDeTarefas.Views
+{
+    partial class TelaTech
+    {
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listTarefas = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(1, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1298, 87);
+            this.panel3.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(907, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Criar Nova Tarefa";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Bem vindo, João Teste";
+            // 
+            // listTarefas
+            // 
+            this.listTarefas.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.listTarefas.Alignment = System.Windows.Forms.ListViewAlignment.SnapToGrid;
+            this.listTarefas.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.listTarefas.CheckBoxes = true;
+            this.listTarefas.GridLines = true;
+            this.listTarefas.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listTarefas.HideSelection = false;
+            this.listTarefas.HotTracking = true;
+            this.listTarefas.HoverSelection = true;
+            this.listTarefas.Location = new System.Drawing.Point(12, 165);
+            this.listTarefas.Name = "listTarefas";
+            this.listTarefas.Size = new System.Drawing.Size(1243, 609);
+            this.listTarefas.TabIndex = 0;
+            this.listTarefas.TileSize = new System.Drawing.Size(10, 10);
+            this.listTarefas.UseCompatibleStateImageBehavior = false;
+            this.listTarefas.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(34, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 54);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Todas";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(189, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 54);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Em Atraso";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(341, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 54);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Em Análise";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(796, 93);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(146, 54);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Concluídas";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(644, 93);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(146, 54);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Abandonadas";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button6.Location = new System.Drawing.Point(492, 93);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(146, 54);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Com Impedimento";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Location = new System.Drawing.Point(12, 120);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1243, 47);
+            this.panel1.TabIndex = 8;
+            // 
+            // TelaDev
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1267, 786);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listTarefas);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "TelaDev";
+            this.Text = "TelaDev";
+            this.Load += new System.EventHandler(this.TelaTech_Load);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listTarefas;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
